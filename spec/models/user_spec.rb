f7b3d04d2ef1702de09541a57e67c72bdb958913
Robert Floor <robertfloor@gmail.com>
2012-10-14 16:46:16 +0200

    Fixed admin tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,8 +29,16 @@ describe User do
               User.new(admin: "1")
            end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
         end
-
     end
+
+ describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
    describe "when name is not present" do
      before {@user.name= " "}
